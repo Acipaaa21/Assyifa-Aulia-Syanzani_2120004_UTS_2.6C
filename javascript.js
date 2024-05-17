9564c7eadbb1c4fb8359a9039f795bb522ef4947
Acipaaa21 <165894008+Acipaaa21@users.noreply.github.com>
2024-05-17 20:42:28 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+<script>
+        function tambah() {
+            var num1 = parseFloat(document.getElementById("num1").value);
+            var num2 = parseFloat(document.getElementById("num2").value);
+            var hasil = num1 + num2;
+            document.getElementById("Hasil Penjumlahan").innerText = hasil;
+        }
+    </script>
